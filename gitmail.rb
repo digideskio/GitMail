a94cf8807408145127ee18f7dfab02b57fb8d004
Frank Hamand <frankhamand@gmail.com>
2016-03-15 17:13:45 +0000

    Use mktmpdir instead of creating temp_email directory
    The Dir.mktmpdir method automatically creates a temporary folder and cleans it up at the end of the block.
    
    Much cleaner than manually creating and deleting the folder

--- gitmail.rb
+++ gitmail.rb
@@ -1,10 +1,11 @@
+require 'tmpdir'
+
 class Finder
   def initialize(repo_name)
-    `git clone https://github.com/#{repo_name}.git temp_email`
-    Dir.chdir("temp_email")
-    @emails = `git log --pretty=format:'%an %ae'`
-    Dir.chdir("..")
-    `rm -rf temp_email`
+    Dir.mktmpdir("GitMail") do |dir|
+      `git clone https://github.com/#{repo_name}.git #{dir}`
+      @emails = `git log --pretty=format:'%an %ae'`
+    end
   end
 
   def all_contributors
